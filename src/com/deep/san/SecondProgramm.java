1d60c116ac161c8fe7de692e49531ab87b14c217
Your Name <you@example.com>
2021-10-07 20:50:53 +0530

    my commit

---
+++ src/com/deep/san/SecondProgramm.java
@@ -0,0 +1,14 @@
+package com.deep.san;
+
+public class SecondProgramm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		
+		System.out.println(a+b);
+
+	}
+
+}
