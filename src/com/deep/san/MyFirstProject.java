17571de8bf1f27b8e67600dd9129c759b66f7d9c
Your Name <you@example.com>
2021-10-07 10:59:15 +0530

    first commit

---
+++ src/com/deep/san/MyFirstProject.java
@@ -0,0 +1,11 @@
+package com.deep.san;
+
+public class MyFirstProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi....github");
+
+	}
+
+}
